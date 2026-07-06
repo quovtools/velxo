82533c07e96f4fbe260c9b4982217ecd8519f5eb
Your Name <quovtools@gmail.com>
2026-07-06 02:11:06 +0100

    fix: add PrismaModule to AuthModule imports

--- backend/src/modules/auth/auth.module.ts
+++ backend/src/modules/auth/auth.module.ts
@@ -5,10 +5,12 @@ import { ConfigModule, ConfigService } from '@nestjs/config'
 import { AuthService } from './auth.service'
 import { AuthController } from './auth.controller'
 import { UsersModule } from '../users/users.module'
+import { PrismaModule } from '../../common/services/prisma.module'
 import { JwtStrategy } from './strategies/jwt.strategy'
 
 @Module({
   imports: [
+    PrismaModule,
     UsersModule,
     PassportModule.register({ defaultStrategy: 'jwt' }),
     JwtModule.registerAsync({
