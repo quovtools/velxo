c65e611f1b63dc42f5d8e534e6e1762020516a16
Your Name <quovtools@gmail.com>
2026-07-18 13:19:08 +0100

    fix: add PrismaModule import to UploadModule to resolve JwtAuthGuard dependency

--- backend/src/modules/upload/upload.module.ts
+++ backend/src/modules/upload/upload.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common'
 import { UploadController } from './upload.controller'
 import { StorageService } from './storage.service'
+import { PrismaModule } from '@/common/services/prisma.module'
 
 @Module({
+  imports: [PrismaModule],
   controllers: [UploadController],
   providers: [StorageService],
   exports: [StorageService],
